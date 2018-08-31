28cb4d91ac22a9733f60296d033d7a5fed265a4f
frederic wagner <frederic.wagner@imag.fr>
2018-08-31 12:06:30 +0200

    merge sort : fusion is missing

--- src/bin/merge.rs
+++ src/bin/merge.rs
@@ -5,9 +5,12 @@ extern crate rayon_adaptive;
 extern crate rayon_logs;
 
 use rand::random;
-use rayon_adaptive::{Divisible, EdibleSlice, EdibleSliceMut, Policy};
+use rayon_adaptive::{Divisible, EdibleSlice, EdibleSliceMut, Mergeable, Policy};
 use rayon_logs::ThreadPoolBuilder;
+use std::cmp::min;
 use std::iter::repeat;
+
+// main related code
 
 /// find subslice without last value in given sorted slice.
 fn subslice_without_last_value<T: Eq>(slice: &[T]) -> &[T] {
@@ -128,6 +131,24 @@ impl<'a, T: 'a + Send + Sync + Ord + Copy> Divisible for FusionSlice<'a, T> {
     }
 }
 
+fn sequential_fuse<T: Ord + Copy>(left: &[T], right: &[T], output: &mut [T]) {
+    let mut left_iterator = left.iter().peekable();
+    let mut right_iterator = right.iter().peekable();
+    for o in output {
+        let go_left = match (left_iterator.peek(), right_iterator.peek()) {
+            (Some(l), Some(r)) => l <= r,
+            (Some(_), None) => true,
+            (None, Some(_)) => false,
+            (None, None) => panic!("not enough input when merging"),
+        };
+        *o = if go_left {
+            *left_iterator.next().unwrap()
+        } else {
+            *right_iterator.next().unwrap()
+        };
+    }
+}
+
 fn fuse<T: Ord + Send + Sync + Copy>(left: &[T], right: &[T], output: &mut [T]) {
     let slices = FusionSlice {
         left: EdibleSlice::new(left),
@@ -158,24 +179,150 @@ fn fuse<T: Ord + Send + Sync + Copy>(left: &[T], right: &[T], output: &mut [T]) 
     );
 }
 
-fn main() {
-    let v: Vec<u32> = (0..10_000_000).collect();
-    let mut left = Vec::new();
-    let mut right = Vec::new();
-    for e in &v {
-        if random() { &mut left } else { &mut right }.push(*e);
+// sort related code
+
+/// We'll need slices of several vectors at once.
+struct SortingSlices<'a, T: 'a> {
+    s: Vec<&'a mut [T]>, // we have 2 input slices and one output slice
+    i: usize,            // index of slice containing the data
+    eaten: usize,        // how much has already been processed
+}
+
+impl<'a, T: 'a> SortingSlices<'a, T> {
+    /// Return the two mutable slices of given indices.
+    fn mut_couple<'b>(&'b mut self, i1: usize, i2: usize) -> (&'b mut [T], &'b mut [T]) {
+        let (s0, s1, s2) = {
+            let (s0, leftover) = self.s.split_first_mut().unwrap();
+            let (s1, s2) = leftover.split_first_mut().unwrap();
+            (s0, s1, s2.get_mut(0).unwrap())
+        };
+        match (i1, i2) {
+            (0, 1) => (s0, s1),
+            (0, 2) => (s0, s2),
+            (1, 0) => (s1, s0),
+            (1, 2) => (s1, s2),
+            (2, 0) => (s2, s0),
+            (2, 1) => (s2, s1),
+            _ => panic!("i1 == i2"),
+        }
     }
-    println!("{}/{}", left.len(), right.len());
-    let mut w = vec![0; 10_000_000];
+    fn split_at(self, i: usize) -> (Self, Self) {
+        let v = self.s.into_iter().map(|s| s.split_at_mut(i)).fold(
+            (Vec::new(), Vec::new()),
+            |mut acc, (s1, s2)| {
+                acc.0.push(s1);
+                acc.1.push(s2);
+                acc
+            },
+        );
+        (
+            SortingSlices {
+                s: v.0,
+                i: self.i,
+                eaten: self.eaten,
+            },
+            SortingSlices {
+                s: v.1,
+                i: self.i,
+                eaten: 0,
+            },
+        )
+    }
+}
+
+impl<'a, T: 'a + Ord + Copy + Sync + Send> Divisible for SortingSlices<'a, T> {
+    fn len(&self) -> usize {
+        self.s[0].len() - self.eaten
+    }
+    fn split(self) -> (Self, Self) {
+        let mid = (self.s[0].len() - self.eaten) / 2;
+        let eaten = self.eaten;
+        self.split_at(eaten + mid)
+    }
+}
+
+impl<'a, T: 'a + Ord + Copy + Sync + Send> Mergeable for SortingSlices<'a, T> {
+    fn fuse(self, other: Self) -> Self {
+        unimplemented!()
+    }
+}
+
+fn adaptive_sort<T: Ord + Copy + Send + Sync>(slice: &mut [T]) {
+    let mut tmp_slice1 = Vec::with_capacity(slice.len());
+    let mut tmp_slice2 = Vec::with_capacity(slice.len());
+    unsafe {
+        tmp_slice1.set_len(slice.len());
+        tmp_slice2.set_len(slice.len());
+    }
+
+    let slices = SortingSlices {
+        s: vec![slice, tmp_slice1.as_mut_slice(), tmp_slice2.as_mut_slice()],
+        i: 0,
+        eaten: 0,
+    };
+
+    let result_slices = slices.work(
+        |slices, limit| {
+            let eaten = slices.eaten;
+            let new_eaten = min(slices.eaten + limit, slices.s[0].len());
+            let input_index = slices.i;
+            let output_index = (input_index + 1) % 3;
+            {
+                let (in_slice, out_slice) = slices.mut_couple(input_index, output_index);
+                in_slice[eaten..new_eaten].sort();
+                sequential_fuse(
+                    &in_slice[0..eaten],
+                    &in_slice[eaten..new_eaten],
+                    &mut out_slice[0..new_eaten],
+                );
+            }
+            slices.i = output_index;
+            slices.eaten = new_eaten;
+        },
+        |s| s,
+        Policy::Adaptive(2_000),
+    );
+
+    unimplemented!()
+}
+
+fn main() {
+    let mut v: Vec<u32> = (0..1_000_000).map(|_| random::<u32>() % 100_000).collect();
+    let mut w = v.clone();
+    w.sort();
+    adaptive_sort(&mut v);
 
     let pool = ThreadPoolBuilder::new()
-        .num_threads(4)
+        .num_threads(1)
         .build()
         .expect("pool creation failed");
     let log = pool.install(|| {
-        fuse(&left, &right, &mut w);
+        adaptive_sort(&mut v);
     }).1;
     log.save_svg("merge.svg").expect("saving svg failed");
 
     assert_eq!(v, w);
 }
+
+// fn main() {
+//
+//     let v: Vec<u32> = (0..10_000_000).collect();
+//     let mut left = Vec::new();
+//     let mut right = Vec::new();
+//     for e in &v {
+//         if random() { &mut left } else { &mut right }.push(*e);
+//     }
+//     println!("{}/{}", left.len(), right.len());
+//     let mut w = vec![0; 10_000_000];
+//
+//     let pool = ThreadPoolBuilder::new()
+//         .num_threads(4)
+//         .build()
+//         .expect("pool creation failed");
+//     let log = pool.install(|| {
+//         fuse(&left, &right, &mut w);
+//     }).1;
+//     log.save_svg("merge.svg").expect("saving svg failed");
+//
+//     assert_eq!(v, w);
+// }
