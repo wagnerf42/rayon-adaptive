2717f2ddba6573103e035071f93aa1d7c2ee1501
frederic wagner <frederic.wagner@imag.fr>
2018-09-20 11:15:29 +0200

    better benches

--- benches/merge_sort.rs
+++ benches/merge_sort.rs
@@ -11,47 +11,58 @@ use rayon_adaptive::adaptive_sort;
 use criterion::Criterion;
 
 fn merge_sort_adaptive(c: &mut Criterion) {
-    let input: Vec<u32> = (0..1_000_000).rev().collect();
     c.bench_function(
         "adaptive merge sort (size=1_000_000, reversed input)",
         move |b| {
-            b.iter(|| {
-                let mut v = input.clone();
-                adaptive_sort(&mut v);
-            })
+            b.iter_with_setup(
+                || (0..1_000_000).rev().collect::<Vec<u32>>(),
+                |mut v| {
+                    adaptive_sort(&mut v);
+                },
+            )
         },
     );
+    let mut all_numbers: Vec<u32> = (0..1_000_000).collect();
+    let mut ra = ChaChaRng::new_unseeded();
     c.bench_function(
         "adaptive merge sort (size=1_000_000, random input)",
         move |b| {
-            let mut ra = ChaChaRng::new_unseeded();
-            b.iter(|| {
-                let mut v: Vec<u32> = (0..1_000_000).collect();
-                ra.shuffle(&mut v);
-                adaptive_sort(&mut v);
-            })
+            b.iter_with_setup(
+                || {
+                    ra.shuffle(&mut all_numbers);
+                    all_numbers.clone()
+                },
+                |mut v| {
+                    adaptive_sort(&mut v);
+                },
+            )
         },
     );
 
-    let input: Vec<u32> = (0..1_000_000).rev().collect();
     c.bench_function(
         "rayon merge sort (size=1_000_000, reversed input)",
         move |b| {
-            b.iter(|| {
-                let mut v = input.clone();
-                v.par_sort();
-            })
+            b.iter_with_setup(
+                || (0..1_000_000).rev().collect::<Vec<u32>>(),
+                |mut v| {
+                    v.par_sort();
+                },
+            )
         },
     );
+    let mut all_numbers: Vec<u32> = (0..1_000_000).collect();
     c.bench_function(
         "rayon merge sort (size=1_000_000, random input)",
         move |b| {
-            let mut ra = ChaChaRng::new_unseeded();
-            b.iter(|| {
-                let mut v: Vec<u32> = (0..1_000_000).collect();
-                ra.shuffle(&mut v);
-                v.par_sort();
-            })
+            b.iter_with_setup(
+                || {
+                    ra.shuffle(&mut all_numbers);
+                    all_numbers.clone()
+                },
+                |mut v| {
+                    v.par_sort();
+                },
+            )
         },
     );
 }
