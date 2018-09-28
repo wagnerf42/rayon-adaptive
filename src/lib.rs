5c470523119f14969b17e26f4326a59fb82b0ec2
Saurabh Raje <saurabh.mraje@gmail.com>
2018-09-28 17:34:23 +0200

    re-organised infix solvers

--- src/lib.rs
+++ src/lib.rs
@@ -12,6 +12,7 @@ pub use utils::fuse_slices;
 mod slices;
 pub use slices::{EdibleSlice, EdibleSliceMut};
 mod algorithms;
+pub use algorithms::infix_solvers::*;
 pub use algorithms::merge_sort::adaptive_sort;
 pub use algorithms::prefix::adaptive_prefix;
 
