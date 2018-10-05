1aa0499afeea21e393fbb29a01b0114dcce6d85e
Saurabh Raje <saurabh.mraje@gmail.com>
2018-10-05 13:54:39 +0200

    baseline

--- src/scheduling.rs
+++ src/scheduling.rs
@@ -311,10 +311,10 @@ where
                 return self.answer_steal();
             }
             //TODO: Remove the cap to restore the original version of rayon-adaptive.
-            self.current_block_size = min(
-                (self.current_block_size as f64 * phi) as usize,
-                self.initial_block_size * 10,
-            );
+            self.current_block_size = //min(
+                (self.current_block_size as f64 * phi) as usize
+                //self.initial_block_size * 10,
+            ;
             let size = min(self.input.len(), self.current_block_size);
 
             if self.input.len() <= self.current_block_size {
