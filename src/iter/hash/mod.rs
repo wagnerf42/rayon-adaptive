874e3532baad9dcfddb721e88333105df9779431
Saurabh Raje <saurabh.mraje@gmail.com>
2018-12-12 17:45:29 +0100

    added par_iter method for hashmap added example for par_iter method for hashmap

--- src/iter/hash/mod.rs
+++ src/iter/hash/mod.rs
@@ -21,3 +21,13 @@ pub fn par_keys<'a, K: Send + Sync + Eq + Hash, V: Send + Sync>(
         .filter(|&(&h, _)| h != 0)
         .map(|(_, &(ref k, _))| k)
 }
+pub fn par_iter<'a, K: Send + Sync + Eq + Hash, V: Send + Sync>(
+    hashmap: &'a HashMap<K, V>,
+) -> impl AdaptiveIterator<Item = (&'a K, &'a V)> {
+    let (hashes, pairs) = unsafe { extract_slices(hashmap) };
+    hashes
+        .into_adapt_iter()
+        .zip(pairs.into_adapt_iter())
+        .filter(|&(&h, _)| h != 0)
+        .map(|(_, &(ref k, ref v))| (k, v))
+}
