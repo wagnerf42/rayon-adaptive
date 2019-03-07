5d85102786e50b3c405460b3f56e93d313271ffc
frederic wagner <frederic.wagner@univ-grenoble-alpes.fr>
2019-03-07 16:28:45 +0100

    prefix algorithm with work

---
+++ src/bin/fully_adaptive_work.rs
@@ -0,0 +1,115 @@
+#[cfg(feature = "logs")]
+extern crate rayon_logs as rayon;
+use derive_divisible::{Divisible, DivisibleAtIndex, DivisibleIntoBlocks};
+use rayon_adaptive::fuse_slices;
+use rayon_adaptive::prelude::*;
+use rayon_adaptive::BlockedPower;
+use std::iter::repeat;
+
+const SIZE: usize = 1_000_000;
+
+struct PrefixSlice<'a, T: 'a + Send + Sync> {
+    slice: &'a mut [T],
+    index: usize,
+}
+
+impl<'a, T: 'a + Send + Sync> Divisible for PrefixSlice<'a, T> {
+    type Power = BlockedPower;
+    fn base_length(&self) -> usize {
+        self.slice.len() - self.index
+    }
+    fn divide(self) -> (Self, Self) {
+        let middle = self.base_length() / 2;
+        let (left, right) = self.slice.split_at_mut(self.index + middle);
+        (
+            PrefixSlice {
+                slice: left,
+                index: self.index,
+            },
+            PrefixSlice {
+                slice: right,
+                index: 0,
+            },
+        )
+    }
+}
+
+impl<'a, T: 'a + Send + Sync> DivisibleIntoBlocks for PrefixSlice<'a, T> {
+    fn divide_at(self, index: usize) -> (Self, Self) {
+        let (left, right) = self.slice.split_at_mut(self.index + index);
+        (
+            PrefixSlice {
+                slice: left,
+                index: self.index,
+            },
+            PrefixSlice {
+                slice: right,
+                index: 0,
+            },
+        )
+    }
+}
+
+fn main() {
+    (1..=4).for_each(|number_of_threads| {
+        let pool = rayon::ThreadPoolBuilder::new()
+            .num_threads(number_of_threads)
+            .build()
+            .expect("Thread pool build failed");
+        let mut input_vector = vec![1.0; SIZE];
+        let expected_result: Vec<_> = vec![1.0; SIZE];
+        let start = time::precise_time_ns();
+        let length = input_vector.len();
+        let input = PrefixSlice {
+            slice: input_vector.as_mut_slice(),
+            index: 0,
+        };
+
+        pool.scope(|s| {
+            input
+                .by_blocks(repeat(length / 10))
+                .work(|mut prefix_slice, limit| {
+                    let previous_value = prefix_slice
+                        .slice
+                        .get(prefix_slice.index - 1)
+                        .cloned()
+                        .unwrap_or(1.0);
+                    prefix_slice.slice[prefix_slice.index..(prefix_slice.index + limit)]
+                        .iter_mut()
+                        .fold(previous_value, |previous_value, e| {
+                            *e *= previous_value;
+                            *e
+                        });
+                    prefix_slice.index += limit;
+                    prefix_slice
+                })
+                .map(|s| s.slice)
+                .helping_cutting_fold(
+                    1.0,
+                    |last_elem_prev_slice, prefix_slice| {
+                        prefix_slice
+                            .slice
+                            .iter_mut()
+                            .fold(last_elem_prev_slice, |c, e| {
+                                *e *= c;
+                                *e
+                            })
+                    },
+                    |last_num, slice| {
+                        if let Some(last_slice_num) = slice.last().cloned() {
+                            s.spawn(move |_| slice.into_adapt_iter().for_each(|e| *e *= last_num));
+                            last_num * last_slice_num
+                        } else {
+                            last_num
+                        }
+                    },
+                )
+        });
+
+        let end = time::precise_time_ns();
+        let time_taken_ms = ((end - start) as f64) / (1e6 as f64);
+        assert_eq!(input_vector, expected_result);
+
+        println!("{}, {}", time_taken_ms, number_of_threads);
+    });
+}
