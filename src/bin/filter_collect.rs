e7a96f5925ea1d4c411fb460dd2001d6feb50658
frederic wagner <frederic.wagner@univ-grenoble-alpes.fr>
2019-04-12 17:15:27 +0200

    filter collect is ok
    retrieving takes a very small fraction of total time
    -> fully adapt cannot beat adapt
    
    moreover I guess we just win by allocating with a smart size

--- src/bin/filter_collect.rs
+++ src/bin/filter_collect.rs
@@ -11,11 +11,54 @@ use rayon_logs::ThreadPoolBuilder;
 #[cfg(feature = "logs")]
 use rayon_logs::scope;
 
+#[cfg(feature = "logs")]
+use rayon_logs::subgraph;
+
+#[cfg(not(feature = "logs"))]
+use rayon::scope;
+
 use rand::random;
 use std::iter::repeat;
 
 fn even(e: &&u32) -> bool {
     **e % 2 == 0
+}
+
+fn fully_adaptive_test(v: &[u32], answer: &[u32]) {
+    let mut a = Vec::with_capacity(2_000_000);
+    unsafe { a.set_len(2_000_000) };
+    scope(|sc| {
+        v.into_adapt_iter()
+            .filter(even)
+            .cloned()
+            .by_blocks(repeat(500_000))
+            .fold(
+                || Vec::with_capacity(500_000),
+                |mut v, e| {
+                    v.push(e);
+                    v
+                },
+            )
+            .helping_fold(
+                (a.as_mut_slice(), 0),
+                |(s, i), e| {
+                    s[i] = e;
+                    (s, i + 1)
+                },
+                |(s, i), v2| {
+                    let (his, mine) = s[i..].split_at_mut(v2.len());
+                    sc.spawn(move |_| {
+                        subgraph("retrieve", v2.len(), || {
+                            for (i, o) in v2.iter().zip(his.iter_mut()) {
+                                *o = *i;
+                            }
+                        })
+                    });
+                    (mine, 0)
+                },
+            );
+    });
+    assert_eq!(a, answer);
 }
 
 fn main() {
@@ -45,41 +88,7 @@ fn main() {
                         .collect::<Vec<_>>()
                 )
             })
-            //            .attach_algorithm("fully_adaptive", || {
-            //                // TODO : crashes because we cannot call the pool's scope inside the install ?
-            //                let mut a = Vec::with_capacity(2_000_000);
-            //                unsafe { a.set_len(2_000_000) };
-            //                pool.scope(|sc| {
-            //                    v.into_adapt_iter()
-            //                        .filter(even)
-            //                        .cloned()
-            //                        .by_blocks(repeat(1_000_000))
-            //                        .fold(
-            //                            || Vec::with_capacity(500_000),
-            //                            |mut v, e| {
-            //                                v.push(e);
-            //                                v
-            //                            },
-            //                        )
-            //                        .helping_fold(
-            //                            (a.as_mut_slice(), 0),
-            //                            |(s, i), e| {
-            //                                s[i] = e;
-            //                                (s, i + 1)
-            //                            },
-            //                            |(s, i), v2| {
-            //                                let (his, mine) = s[i..].split_at_mut(v2.len());
-            //                                sc.spawn(move |_| {
-            //                                    for (i, o) in v2.iter().zip(his.iter_mut()) {
-            //                                        *o = *i;
-            //                                    }
-            //                                });
-            //                                (mine, 0)
-            //                            },
-            //                        );
-            //                });
-            //                assert_eq!(a, answer);
-            //            })
+            .attach_algorithm("fully_adaptive", || fully_adaptive_test(&v, &answer))
             .generate_logs("filter_collect.html")
             .expect("generating logs failed");
     }
