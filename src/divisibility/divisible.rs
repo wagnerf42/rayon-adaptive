ea56ee2b35c1fffeafa2c2a8fdff1e514fcb6c13
frederic wagner <frederic.wagner@univ-grenoble-alpes.fr>
2019-05-02 15:14:22 +0200

    blocks iterator: no more unsafe

--- src/divisibility/divisible.rs
+++ src/divisibility/divisible.rs
@@ -1,6 +1,5 @@
 use crate::Policy;
-use std::iter::empty;
-use std::ptr;
+use std::mem;
 
 /// This is the first level of the divisibility traits hierarchy.
 /// All parallel objects must at least implement this trait.
@@ -19,29 +18,39 @@ pub trait Divisible: Sized {
     }
 }
 
-/// Iterator on some `Divisible` input by blocks by blocks.
+/// Iterator on some `Divisible` input by blocks.
 pub struct BlocksIterator<I: DivisibleIntoBlocks> {
     /// sizes of all the remaining blocks
     sizes: Box<Iterator<Item = usize>>,
     /// remaining input
-    remaining: I,
+    remaining: Option<I>,
 }
 
 impl<I: DivisibleIntoBlocks> Iterator for BlocksIterator<I> {
     type Item = I;
     fn next(&mut self) -> Option<Self::Item> {
-        let remaining_length = self.remaining.base_length();
+        if self.remaining.is_none() {
+            // no input left
+            return None;
+        }
+
+        let remaining_length = self.remaining.as_ref().base_length();
         if let Some(length) = remaining_length {
             if length == 0 {
+                // no input left
                 return None;
             }
         }
+
         let current_size = self.sizes.next();
         if let Some(size) = current_size {
-            let current_block = self.remaining.cut_left_at(size);
-            Some(current_block)
+            let remaining_input = self.remaining.take().unwrap();
+            let (left, right) = remaining_input.divide_at(size);
+            mem::replace(&mut self.remaining, Some(right));
+            Some(left)
         } else {
-            None
+            // no sizes left, return everything thats left to process
+            self.remaining.take()
         }
     }
 }
@@ -57,30 +66,19 @@ pub trait DivisibleIntoBlocks: Sized {
     fn base_length(&self) -> Option<usize>;
     /// Cut the `Divisible` into two parts at specified index.
     fn divide_at(self, index: usize) -> (Self, Self);
-    /// Divide ourselves keeping right part in self.
-    /// Returns the left part.
-    /// NB: this is useful for iterators creation.
-    fn cut_left_at(&mut self, index: usize) -> Self {
-        // there is a lot of unsafe going on here.
-        // I think it's ok. rust uses the same trick for moving iterators (vecs for example)
-        unsafe {
-            let my_copy = ptr::read(self);
-            let (left, right) = my_copy.divide_at(index);
-            let pointer_to_self = self as *mut Self;
-            ptr::write(pointer_to_self, right);
-            left
-        }
-    }
-
     /// Return current scheduling `Policy`.
     fn policy(&self) -> Policy {
         Policy::Rayon
     }
     /// Iterate on all blocks.
     fn blocks(self) -> BlocksIterator<Self> {
+        let sizes = Box::new(self.base_length().into_iter());
         BlocksIterator {
-            remaining: self,
-            sizes: Box::new(empty()),
+            remaining: Some(self),
+            sizes,
         }
     }
 }
+
+/// Implement for zippable stuff.
+pub trait DivisibleAtIndex: DivisibleIntoBlocks {}
