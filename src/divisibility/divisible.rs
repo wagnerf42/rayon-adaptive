c88c0699b92ffd4a5a1fa117fa66449ae7a93014
frederic wagner <frederic.wagner@univ-grenoble-alpes.fr>
2019-06-04 17:26:36 +0200

    documenting divisible

--- src/divisibility/divisible.rs
+++ src/divisibility/divisible.rs
@@ -29,19 +29,65 @@ pub trait BlockedPowerOrMore: Power {}
 impl BlockedPowerOrMore for BlockedPower {}
 impl BlockedPowerOrMore for IndexedPower {}
 
-/// This is the first level of the divisibility traits hierarchy.
-/// All parallel objects must at least implement this trait.
-/// Note that this abstraction is stronger than parallel iterators and
-/// will allow parallel operations on non-iterator objects.
+/// All parallelism provided by this library is an abstraction over divide and conquer.
+/// Parallel iterators can be divided into smaller parallel iterators but not only them,
+/// we can provide parallel algorithms on objects which are NOT iterators like slices.
+///
+/// The main trait enabling us this abstraction is the `Divisible` trait enabling us
+/// to divide an object into independant pieces.
+///
+/// All parallel objects implement this trait : all parallel iterators, slices, ranges, options,...
+///
+/// Not that ALL divisible types can reach a size of 0 which means division will never fail.
+///
+/// It directly comes with parallel algorithms even if the `Divisible` type is not an iterator.
 pub trait Divisible: Sized {
-    /// What we can really do.
+    /// In rayon-adaptive we distinguish between several type of divisibility.
+    /// The most basic object can be split in two on demand but no control
+    /// is possible on the split point.
+    /// Think for example of a parallel iterator on a binary tree.
+    /// You can split only into left and right subtrees.
+    ///
+    /// Then above that you can get some control about the load balancing by dividing at
+    /// around a given index. Think for example of a parallel iterator on keys of a hash table.
+    /// Even if you do not know how many keys are available in the table you can still split
+    /// exactly where you want in the table.
+    ///
+    /// The last type of `Divisible` objects can be split with an exact control
+    /// of the sizes of the obtained pieces (range or slice and their corresponding iterators).
+    ///
+    /// We specialize many algorithms to get higher performances according to how your types allow
+    /// to be divided. For example collecting from an indexed iterator into a vector is very fast but collecting
+    /// from a non-indexed (filtered for example) iterator can be much slower.
+    /// In order to choose what is the best algorithm AT COMPILE TIME we need to remember what is
+    /// the true Power of the type.
     type Power: Power;
-    /// Return our size. This corresponds to the number of operations to be issued.
-    /// For example *i.filter(f)* should have as size the number of elements in i before
-    /// filtering. At size 0 nothing is left to do.
-    /// Return None if size is infinite.
+
+    /// The `base_length` function is both very simple and sometimes quite subtle to implement.
+    /// A `Divisible` type needs to provide us with a size corresponding to the amount of
+    /// iterations it contains. This information is crucial for inner scheduling algorithms.
+    ///
+    /// The size returned is an Option because we allow types of potentially infinite sizes like
+    /// `Repeat` or infinite ranges. This types should return None to signal an unbounded size.
+    ///
+    /// The rules about the sizes returned are the following :
+    /// - performances are best if you know and say the amount of work (usually iterations) inside
+    /// self
+    /// - if you say 0 it means all computations are now completed
+    /// - it does not need to be a decreasing function : for example when using a flat_map we start
+    /// with sizes of the outer parallel iterator but once we start parallelising the inner
+    /// iterator we switche also to its sizes. It's then perfectly possible to have an object of
+    /// size 2 being divided into two objects of larger sizes.
     fn base_length(&self) -> Option<usize>;
+
+    /// Cut the `Divisible` into two parts, if possible at given index.
+    /// You must implement this function even for low-power types (just discard the index).
+    /// All algorithms will guarantee you that they will never call with and index > base_length.
+    /// The index can be 0 though.
+    fn divide_at(self, index: usize) -> (Self, Self);
+
     /// Cut the `Divisible` into two parts.
+    /// By default we cut at midpoint of base_length.
     fn divide(self) -> (Self, Self) {
         let mid = self
             .base_length()
@@ -49,8 +95,7 @@ pub trait Divisible: Sized {
             / 2;
         self.divide_at(mid)
     }
-    /// Cut the `Divisible` into two parts, if possible at given index.
-    fn divide_at(self, index: usize) -> (Self, Self);
+
     /// Return a sequential iterator on blocks of Self of given sizes.
     fn blocks<S: Iterator<Item = usize>>(self, sizes: S) -> BlocksIterator<Self, S> {
         BlocksIterator {
@@ -58,18 +103,31 @@ pub trait Divisible: Sized {
             remaining: Some(self),
         }
     }
+
     /// Work on ourselves piece by piece until length reaches 0.
+    /// This is a complex algorithm. See `examples/prefix.rs`.
     fn work<W: Fn(Self, usize) -> Self + Send + Clone>(self, work_op: W) -> Work<Self, W> {
         Work {
             remaining_input: Some(self),
             work_op,
         }
     }
-    /// Get a parallel iterator on parts of self.
+
+    /// Get a parallel iterator on parts of self divided by the load balancing algorithm.
+    /// See `example/cut.rs`.
     fn cut(self) -> Cut<Self> {
         Cut { input: self }
     }
-    /// Let's work, helping the sequential thread.
+
+    /// This is the most complex operation available in the library.
+    /// One thread will work sequentially with an optimal algorithm while
+    /// all the other become helper threads and do a different operation.
+    ///
+    /// In this version of this algorithm helper threads do not work on iterators
+    /// but directly consume the `Divisible`.
+    /// See `examples/adaptive_prefix.rs`.
+    ///
+    /// This function registers the helper threads operations.
     fn with_help_work<H>(self, help_op: H) -> HelpWork<Self, H>
     where
         H: Fn(Self, usize) -> Self,
