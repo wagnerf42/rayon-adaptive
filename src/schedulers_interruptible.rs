6c82002dfccb531da7d6539335cceff2f30eedc5
frederic wagner <frederic.wagner@univ-grenoble-alpes.fr>
2019-06-24 11:00:51 +0200

    removed logs from the schedulers

--- src/schedulers_interruptible.rs
+++ src/schedulers_interruptible.rs
@@ -258,28 +258,9 @@ where
             .try_fold((iterator, output), |(mut iterator, output), s| {
                 let checked_size = min(s, iterator.base_length().expect("infinite iterator"));
                 let mut sequential_iterator = iterator.extract_iter(checked_size);
-                let new_output;
-                if not_failed.load(Ordering::Relaxed) {
-                    #[cfg(feature = "logs")]
+                let new_output = if not_failed.load(Ordering::Relaxed) {
                     {
-                        new_output = rayon_logs::subgraph("adaptive block", checked_size, || {
-                            match output.into_result() {
-                                Ok(e) => try_fold(&mut sequential_iterator, e, |b, i| {
-                                    match i.into_result() {
-                                        Ok(t) => op(b, t),
-                                        Err(e) => {
-                                            not_failed.store(false, Ordering::Relaxed);
-                                            I::Item::from_error(e)
-                                        }
-                                    }
-                                }),
-                                Err(e) => I::Item::from_error(e),
-                            }
-                        })
-                    }
-                    #[cfg(not(feature = "logs"))]
-                    {
-                        new_output = match output.into_result() {
+                        match output.into_result() {
                             Ok(e) => try_fold(&mut sequential_iterator, e, |b, i| {
                                 match i.into_result() {
                                     Ok(t) => op(b, t),
@@ -293,8 +274,8 @@ where
                         }
                     }
                 } else {
-                    new_output = output // TODO ?
-                }
+                    output
+                };
                 if iterator
                     .base_length()
                     .expect("running on infinite iterator")
@@ -403,9 +384,7 @@ where
 fn test_all_adaptative() {
     assert!(!(1u64..10_000).into_par_iter().all(|x| x != 8_500));
     assert!((1u64..10_000).into_par_iter().all(|x| x > 0));
-    assert!(!(1u64..50_000)
-        .into_par_iter()
-        .all(|x| x != 47_000));
+    assert!(!(1u64..50_000).into_par_iter().all(|x| x != 47_000));
 }
 
 #[test]
