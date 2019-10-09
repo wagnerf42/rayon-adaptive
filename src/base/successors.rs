835b3de5b670058e93a25bd25e4ba422b8aaaae0
frederic wagner <frederic.wagner@univ-grenoble-alpes.fr>
2019-10-09 11:20:56 +0200

    cleaned successors a little bit
    it seems to work

--- src/base/successors.rs
+++ src/base/successors.rs
@@ -15,13 +15,9 @@ pub struct Successors<T, F, S> {
     skip: S,
 }
 
-// we use a trick to avoid all cloning.
-// if owner_next is not None this is the one we use,
-// else we use our next.
 pub struct BorrowedSuccessors<'a, T: Sync, F: Sync, S: Sync> {
     count: usize,
-    next: Option<T>,
-    owner_next: Option<DislocatedMut<'a, T>>, // we might or might not have a pointer to the owner's next
+    next: either::Either<T, DislocatedMut<'a, T>>, // after division we might own it
     succ: Dislocated<'a, F>,
     skip: Dislocated<'a, S>,
 }
@@ -88,8 +84,7 @@ where
     fn par_borrow<'e>(&'e mut self, size: usize) -> <Self as ParBorrowed<'e>>::Iter {
         BorrowedSuccessors {
             count: size,
-            next: None,
-            owner_next: Some(DislocatedMut::new(&mut self.next)),
+            next: either::Right(DislocatedMut::new(&mut self.next)),
             succ: Dislocated::new(&self.succ),
             skip: Dislocated::new(&self.skip),
         }
@@ -108,10 +103,9 @@ where
     fn seq_borrow<'e>(&'e mut self, size: usize) -> <Self as SeqBorrowed<'e>>::Iter {
         self.count -= size;
         SeqSuccessors {
-            next: if let Some(on) = self.owner_next.as_mut() {
-                on.borrow_mut()
-            } else {
-                DislocatedMut::new(self.next.as_mut().unwrap())
+            next: match &mut self.next {
+                either::Either::Left(n) => DislocatedMut::new(n),
+                either::Either::Right(n) => n.borrow_mut(),
             },
             succ: self.succ.clone(),
         }
@@ -144,33 +138,26 @@ where
     fn divide(mut self) -> (Self, Self) {
         let left_count = self.count / 2;
         let right_count = self.count - left_count;
-        if let Some(mut owner_next) = self.owner_next.take() {
-            // ok, the owner_next is moving towards the right part
-            let mut right_one = (self.skip)(&owner_next, left_count);
-            std::mem::swap(&mut right_one, &mut owner_next);
-            let right = BorrowedSuccessors {
-                count: right_count,
-                next: None,
-                owner_next: Some(owner_next),
-                succ: self.succ.clone(),
-                skip: self.skip.clone(),
-            };
-            self.count = left_count;
-            self.next = Some(right_one);
-            (self, right)
-        } else {
-            // easy case
-            let right_next = (self.skip)(self.next.as_ref().unwrap(), left_count);
-            self.count = left_count;
-            let right = BorrowedSuccessors {
-                count: right_count,
-                next: Some(right_next),
-                owner_next: None,
-                succ: self.succ.clone(),
-                skip: self.skip.clone(),
-            };
-            (self, right)
-        }
+        let (left_next, right_next) = match self.next {
+            either::Either::Left(n) => {
+                let right_next = (self.skip)(&n, left_count);
+                (either::Either::Left(n), either::Either::Left(right_next))
+            }
+            either::Either::Right(mut n) => {
+                let mut right_next = (self.skip)(&n, left_count);
+                std::mem::swap(&mut right_next, &mut n); // put it in owner's next
+                (either::Either::Left(right_next), either::Either::Right(n))
+            }
+        };
+        let right = BorrowedSuccessors {
+            count: right_count,
+            next: right_next,
+            succ: self.succ.clone(),
+            skip: self.skip.clone(),
+        };
+        self.count = left_count;
+        self.next = left_next;
+        (self, right)
     }
 }
 
@@ -185,8 +172,8 @@ where
 /// // let's fake a range just for testing
 /// let s:u64 = successors(0u64,
 ///                    |&i| i+1,
-///                    |&i, n| i+(n as u64)).take(10_000_000).sum();
-/// assert_eq!(s, 5_000_000 * 9_999_999)
+///                    |&i, n| i+(n as u64)).take(10_000).sum();
+/// assert_eq!(s, 5_000* 9_999)
 /// ```
 pub fn successors<T, F, S>(first: T, succ: F, skip: S) -> Successors<T, F, S>
 where
