5d4286eead16f82e86bafa03eed9dd3a2319d3cf
smr97 <saurabh.mraje@gmail.com>
2020-01-20 21:39:02 +0100

    added comparator for sorting. Need to check the runtime: probably includes setup

---
+++ examples/compare_sorts.rs
@@ -0,0 +1,42 @@
+use rand::prelude::*;
+use rand::{thread_rng, Rng};
+use rayon_adaptive::{merge_sort_adaptive_jp, merge_sort_adaptive_rayon};
+#[cfg(feature = "logs")]
+use rayon_logs::ThreadPoolBuilder;
+
+fn main() {
+    #[cfg(feature = "logs")]
+    {
+        let p = ThreadPoolBuilder::new()
+            .num_threads(4)
+            .build()
+            .expect("builder failed");
+        p.compare()
+            .attach_algorithm_with_setup(
+                "sorting with rayon policy only",
+                || {
+                    let mut input = (1..100_001u32).rev().collect::<Vec<u32>>();
+                    input.shuffle(&mut thread_rng());
+                    input
+                },
+                |mut unsorted_vector| {
+                    merge_sort_adaptive_rayon(&mut unsorted_vector);
+                    unsorted_vector
+                },
+            )
+            .attach_algorithm_with_setup(
+                "sorting with a fixed join policy",
+                || {
+                    let mut input = (1..100_001u32).rev().collect::<Vec<u32>>();
+                    input.shuffle(&mut thread_rng());
+                    input
+                },
+                |mut unsorted_vector| {
+                    merge_sort_adaptive_jp(&mut unsorted_vector);
+                    unsorted_vector
+                },
+            )
+            .generate_logs("sort_comparison.html")
+            .expect("logging failed");
+    }
+}
