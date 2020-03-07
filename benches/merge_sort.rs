a32a9bb0aedd7b0f6df3e88d0884ee402ecc8c7c
smr97 <saurabh.mraje@gmail.com>
2020-03-07 16:34:41 +0100

    cherrypicked bench and mergesort from schedulable branch

--- benches/merge_sort.rs
+++ benches/merge_sort.rs
@@ -6,12 +6,13 @@ extern crate rayon_adaptive;
 
 use rayon::prelude::*;
 use rayon_adaptive::merge_sort_adaptive;
-//use thread_binder::ThreadPoolBuilder;
+use thread_binder::ThreadPoolBuilder;
 
 use criterion::{Criterion, ParameterizedBenchmark};
+const NUM_THREADS: usize = 16;
 
 fn merge_sort_benchmarks(c: &mut Criterion) {
-    let sizes = vec![50_000, 100_000, 150_000, 262_144, 1_000_000];
+    let sizes: Vec<u32> = vec![100_000, 1_000_000, 10_000_000, 50_000_000, 100_000_000];
     c.bench(
         "merge sort (random input)",
         ParameterizedBenchmark::new(
@@ -34,8 +35,8 @@ fn merge_sort_benchmarks(c: &mut Criterion) {
         .with_function("adaptive sort", |b, input_size| {
             b.iter_with_setup(
                 || {
-                    let thread_pool = rayon::ThreadPoolBuilder::new()
-                        .num_threads(1)
+                    let thread_pool = ThreadPoolBuilder::new()
+                        .num_threads(NUM_THREADS)
                         .build()
                         .expect("Thread binder didn't work!");
                     (
@@ -56,8 +57,8 @@ fn merge_sort_benchmarks(c: &mut Criterion) {
         .with_function("rayon", |b, input_size| {
             b.iter_with_setup(
                 || {
-                    let thread_pool = rayon::ThreadPoolBuilder::new()
-                        .num_threads(1)
+                    let thread_pool = ThreadPoolBuilder::new()
+                        .num_threads(NUM_THREADS)
                         .build()
                         .expect("Thread binder didn't work!");
                     (
@@ -77,41 +78,61 @@ fn merge_sort_benchmarks(c: &mut Criterion) {
         }),
     );
 
-    //c.bench(
-    //    "merge sort (reversed input)",
-    //    ParameterizedBenchmark::new(
-    //        "sequential",
-    //        |b, input_size| {
-    //            b.iter_with_setup(
-    //                || (0..*input_size).rev().collect::<Vec<u32>>(),
-    //                |mut v| {
-    //                    v.sort();
-    //                    v
-    //                },
-    //            )
-    //        },
-    //        sizes,
-    //    )
-    //    .with_function("adaptive", |b, input_size| {
-    //        b.iter_with_setup(
-    //            || (0..*input_size).rev().collect::<Vec<u32>>(),
-    //            |mut v| {
-    //                merge_sort_adaptive(&mut v);
-    //                v
-    //            },
-    //        )
-    //    })
-    //    .with_function("rayon", |b, input_size| {
-    //        b.iter_with_setup(
-    //            || (0..*input_size).rev().collect::<Vec<u32>>(),
-    //            |mut v| {
-    //                v.par_sort();
-    //                v
-    //            },
-    //        )
-    //    }),
-    //);
+    c.bench(
+        "merge sort (reversed input)",
+        ParameterizedBenchmark::new(
+            "sequential",
+            |b, input_size| {
+                b.iter_with_setup(
+                    || (0u32..*input_size).rev().collect::<Vec<u32>>(),
+                    |mut v| {
+                        v.sort();
+                        v
+                    },
+                )
+            },
+            sizes,
+        )
+        .with_function("adaptive", |b, input_size| {
+            b.iter_with_setup(
+                || {
+                    let thread_pool = ThreadPoolBuilder::new()
+                        .num_threads(NUM_THREADS)
+                        .build()
+                        .expect("Thread binder didn't work!");
+                    (thread_pool, (0u32..*input_size).rev().collect::<Vec<u32>>())
+                },
+                |(tp, mut v)| {
+                    tp.install(|| {
+                        merge_sort_adaptive(&mut v);
+                    });
+                    v
+                },
+            )
+        })
+        .with_function("rayon", |b, input_size| {
+            b.iter_with_setup(
+                || {
+                    let thread_pool = ThreadPoolBuilder::new()
+                        .num_threads(NUM_THREADS)
+                        .build()
+                        .expect("Thread binder didn't work!");
+                    (thread_pool, (0..*input_size).rev().collect::<Vec<u32>>())
+                },
+                |(tp, mut v)| {
+                    tp.install(|| {
+                        v.par_sort();
+                    });
+                    v
+                },
+            )
+        }),
+    );
 }
 
-criterion_group!(benches, merge_sort_benchmarks);
+criterion_group! {
+    name = benches;
+            config = Criterion::default().sample_size(10).nresamples(10);
+                targets = merge_sort_benchmarks
+}
 criterion_main!(benches);
