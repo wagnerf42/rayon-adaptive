cfbef51ff380e9f4eb3bbb947519b549b87ef4cc
frederic wagner <frederic.wagner@imag.fr>
2020-04-02 11:22:24 +0200

    I'm getting there with the sequential iterators
    now, I just need to move this code inside our adaptive iterator.
    
    and of course check the divisions code ;-)

--- benches/merge.rs
+++ benches/merge.rs
@@ -11,6 +11,62 @@ use rayon_adaptive::prelude::*;
 
 use criterion::{Criterion, ParameterizedBenchmark};
 
+struct SliceIter<'a, T: 'a> {
+    slice: &'a [T],
+    index: usize,
+}
+
+impl<'a, T: 'a> SliceIter<'a, T> {
+    fn new(slice: &'a [T]) -> Self {
+        SliceIter { slice, index: 0 }
+    }
+    fn completed(&self) -> bool {
+        self.slice.len() <= self.index
+    }
+    fn peek(&self) -> &T {
+        unsafe { self.slice.get_unchecked(self.index) }
+    }
+}
+
+impl<'a, T: 'a> Iterator for SliceIter<'a, T> {
+    type Item = &'a T;
+    fn next(&mut self) -> Option<Self::Item> {
+        if self.completed() {
+            None
+        } else {
+            let index = self.index;
+            self.index += 1;
+            Some(unsafe { self.slice.get_unchecked(index) })
+        }
+    }
+}
+
+struct MergeIter<'a, T: 'a> {
+    left: SliceIter<'a, T>,
+    right: SliceIter<'a, T>,
+}
+
+impl<'a, T: 'a> MergeIter<'a, T> {
+    fn new(left: SliceIter<'a, T>, right: SliceIter<'a, T>) -> Self {
+        MergeIter { left, right }
+    }
+}
+
+impl<'a, T: 'a + Ord> Iterator for MergeIter<'a, T> {
+    type Item = &'a T;
+    fn next(&mut self) -> Option<Self::Item> {
+        if self.left.completed() {
+            self.right.next()
+        } else if self.right.completed() {
+            self.left.next()
+        } else if self.left.peek() <= self.right.peek() {
+            self.left.next()
+        } else {
+            self.right.next()
+        }
+    }
+}
+
 fn safe_manual_merge(left: &[u32], right: &[u32], output: &mut [u32]) {
     let sizes = [left.len(), right.len()];
     let inputs = [left, right];
@@ -73,6 +129,29 @@ fn unsafe_manual_merge2(left: &[u32], right: &[u32], output: &mut [u32]) {
     }
 }
 
+fn manual_slice_iter(left: &[u32], right: &[u32], output: &mut [u32]) {
+    let mut left_iter = SliceIter::new(left);
+    let mut right_iter = SliceIter::new(right);
+    for o in output {
+        if left_iter.completed() {
+            *o = *right_iter.next().unwrap();
+        } else if right_iter.completed() {
+            *o = *left_iter.next().unwrap();
+        } else if left_iter.peek() <= right_iter.peek() {
+            *o = *left_iter.next().unwrap(); // TODO: remove unwrap ?
+        } else {
+            *o = *right_iter.next().unwrap();
+        };
+    }
+}
+
+fn manual_merge_iter(left: &[u32], right: &[u32], output: &mut [u32]) {
+    let left_iter = SliceIter::new(left);
+    let right_iter = SliceIter::new(right);
+    let mut merge_iter = MergeIter::new(left_iter, right_iter);
+    output.iter_mut().zip(merge_iter).for_each(|(o, i)| *o = *i);
+}
+
 //TODO: this will be very bad if one block ends up being small
 // we should fall back to another algorithm in this case
 fn unsafe_very_manual_merge(left: &[u32], right: &[u32], mut output: &mut [u32]) {
@@ -133,24 +212,42 @@ fn merge_benchmarks(c: &mut Criterion) {
             },
             sizes.clone(),
         )
-        .with_function("safe manual merge", |b, input_size| {
+//        .with_function("safe manual merge", |b, input_size| {
+//            b.iter_with_setup(
+//                || interleaved_input(*input_size),
+//                |(left, right, mut output)| {
+//                    safe_manual_merge(&left, &right, &mut output);
+//                    (left, right, output)
+//                },
+//            )
+//        })
+        .with_function("manual slice iter", |b, input_size| {
             b.iter_with_setup(
                 || interleaved_input(*input_size),
                 |(left, right, mut output)| {
-                    safe_manual_merge(&left, &right, &mut output);
+                    manual_slice_iter(&left, &right, &mut output);
                     (left, right, output)
                 },
             )
         })
-        .with_function("unsafe manual merge", |b, input_size| {
+        .with_function("manual merge iter", |b, input_size| {
             b.iter_with_setup(
                 || interleaved_input(*input_size),
                 |(left, right, mut output)| {
-                    unsafe_manual_merge(&left, &right, &mut output);
+                    manual_merge_iter(&left, &right, &mut output);
                     (left, right, output)
                 },
             )
         })
+//        .with_function("unsafe manual merge", |b, input_size| {
+//            b.iter_with_setup(
+//                || interleaved_input(*input_size),
+//                |(left, right, mut output)| {
+//                    unsafe_manual_merge(&left, &right, &mut output);
+//                    (left, right, output)
+//                },
+//            )
+//        })
         .with_function("unsafe manual merge 2", |b, input_size| {
             b.iter_with_setup(
                 || interleaved_input(*input_size),
@@ -169,27 +266,27 @@ fn merge_benchmarks(c: &mut Criterion) {
                 },
             )
         })
-        .with_function("adaptive generic merge", |b, input_size| {
-            b.iter_with_setup(
-                || {
-                    let thread_pool = rayon::ThreadPoolBuilder::new()
-                        .num_threads(1)
-                        .build()
-                        .expect("Thread pool creation failed!");
-                    (thread_pool, interleaved_input(*input_size))
-                },
-                |(tp, (left, right, mut output))| {
-                    // TODO: I wonder if the install is not taking time here
-                    tp.install(|| {
-                        left.par_iter()
-                            .merge(right.par_iter())
-                            .directional_zip(output.par_iter_mut())
-                            .for_each(|(i, o)| *o = *i)
-                    });
-                    (left, right, output)
-                },
-            )
-        }),
+//        .with_function("adaptive generic merge", |b, input_size| {
+//            b.iter_with_setup(
+//                || {
+//                    let thread_pool = rayon::ThreadPoolBuilder::new()
+//                        .num_threads(1)
+//                        .build()
+//                        .expect("Thread pool creation failed!");
+//                    (thread_pool, interleaved_input(*input_size))
+//                },
+//                |(tp, (left, right, mut output))| {
+//                    // TODO: I wonder if the install is not taking time here
+//                    tp.install(|| {
+//                        left.par_iter()
+//                            .merge(right.par_iter())
+//                            .directional_zip(output.par_iter_mut())
+//                            .for_each(|(i, o)| *o = *i)
+//                    });
+//                    (left, right, output)
+//                },
+//            )
+//        }),
     );
 }
 
